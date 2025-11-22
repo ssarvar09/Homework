13515eebca119732e9b64e6d18c21df82af8f656
ssarvar09 <sarvarquryazov0@gmail.com>
2025-11-22 16:43:27 +0300

    Create homework18.py

---
+++ homework18.py
@@ -0,0 +1,76 @@
+# def user_data(first_name, last_name, age):
+#     return {
+#         'ism': first_name,
+#         'familiya': last_name,
+#         'yosh': age,
+#     }
+# ism = input('ismingniz kiriting:')
+# familiya =input('familiyanginz kiriting:')
+# yosh = input('yoshingizni kiriting:')
+# i = user_data(ism,familiya,yosh)
+#
+# print('Ism:',i['ism'])
+# print('Familiya:',i['familiya'])
+# print('Yosh:',i['yosh'])
+
+
+# def find_max(a,b,c):
+#     max_value = max(a,b,c)
+#     k=[]
+#     if a == max_value:
+#        k.append('A')
+#     if b == max_value:
+#        k.append('B')
+#     if c == max_value:
+#        k.append("C")
+#
+#     if len(k) ==1:
+#         print(f"Eng katta son - {k[0]} {max_value}")
+#     elif len(k) ==2:
+#         print(f"Eng katta son - {k[0]} va {k[1]} {max_value}")
+#     else:
+#         print(f"eng katta son - A  va B va C {max_value}")
+#
+# a = int(input(" A sonni kiriting:"))
+# b = int(input(" B sonni kiriting:"))
+# c = int(input(" C sonni kiriting:"))
+# find_max(a,b,c)
+
+
+# ### 3
+# def find_letter_count(word, letter):
+#     c = word.count(letter)
+#     print(f'"{word}" soʻzida "{letter}" harfi {c} marta qatnashgan.')
+#
+# # Foydalanuvchidan soʻz va harfni olish
+# word = input("Soʻz kiriting: ")
+# letter = input("Qaysi harfni sanaymiz: ")
+# find_letter_count(word, letter)
+#
+
+
+
+### 4
+# def list_sum(myList):
+#     t = sum(myList)
+#     print(f"Listning elementlar yig'indisi = {t}")
+# list_sum([5, 7, 10, 10])
+
+
+
+### 5
+# def daraja(a, b):
+#     s = a ** b
+#     print(f"{a} ning {b}-darajasi = {s}")
+# daraja(5, 2)
+#
+#
+# ### 6
+#
+# def daraja4(a, b, c, d):
+#     print(f"{a} ning {b}-darajasi = {a ** b}")
+#     print(f"{a} ning {c}-darajasi = {a ** c}")
+#     print(f"{a} ning {d}-darajasi = {a ** d}")
+# araja4(2, 3, 4, 5)
+
+d
